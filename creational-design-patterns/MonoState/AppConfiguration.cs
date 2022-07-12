7e17e5115af7626463f9fc6a721e9caacd30216a
Alan Falcoski Rodrigues <33583484+alanfalcoski@users.noreply.github.com>
2022-07-11 23:56:24 -0300

    MonoState alternative to Singleton

---
+++ creational-design-patterns/MonoState/AppConfiguration.cs
@@ -0,0 +1,22 @@
+﻿namespace MonoState
+{
+    public class AppConfig
+    {
+        private static String key;
+
+        public String getData()
+        {
+            return AppConfig.key;
+        }
+
+        public void setKey(string value)
+        {
+            AppConfig.key = value;
+        }
+
+        public AppConfig()
+        {
+            
+        }
+    }
+}
---
+++ creational-design-patterns/MonoState/Program.cs
@@ -0,0 +1,19 @@
+﻿using MonoState;
+
+/* 
+   MonoState - Com um atributo static simula o comportamento do Singletom.
+   Com 'n' instâncias criadas retornará sempre o último valor setado.   
+*/
+
+AppConfig appConfig = new AppConfig();
+appConfig.setKey("value");
+AppConfig appConfig2 = new AppConfig();
+
+Console.WriteLine(appConfig.getData());
+Console.WriteLine(appConfig2.getData());
+
+appConfig2.setKey("value again");
+Console.WriteLine(appConfig.getData());
+Console.WriteLine(appConfig2.getData());
+
+Console.ReadKey();
